fa07bb019ba0fead9abb3b616e8a0fffd7660149
Hai Doan Duc <beu2901@gmail.com>
2025-07-20 18:48:33 +0700

    update login window

---
+++ FarmProductsWPF_Repositories/Implements/AccountRepo.cs
@@ -0,0 +1,20 @@
+﻿using FarmProductsWPF_BOs;
+using FarmProductsWPF_DAOs;
+using FarmProductsWPF_Repositories.Interfaces;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmProductsWPF_Repositories.Implements
+{
+    public class AccountRepo : IAccountRepo
+    {
+        public Account? GetAccountByLogin(string username, string password)
+        {
+            return AccountDAO.Instance.GetAccountByLogin(username, password);
+        }
+    }
+}
