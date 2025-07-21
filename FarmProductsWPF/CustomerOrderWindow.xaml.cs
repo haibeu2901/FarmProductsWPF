18e36284248377d20c8c819cabd8ebc6438f7e88
Hai Doan Duc <beu2901@gmail.com>
2025-07-21 22:37:30 +0700

    add ui for customer

---
+++ FarmProductsWPF/CustomerOrderWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FarmProductsWPF
+{
+    /// <summary>
+    /// Interaction logic for CustomerOrderWindow.xaml
+    /// </summary>
+    public partial class CustomerOrderWindow : Window
+    {
+        public CustomerOrderWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            this.Close();
+            mainWindow.Show();
+        }
+    }
+}
