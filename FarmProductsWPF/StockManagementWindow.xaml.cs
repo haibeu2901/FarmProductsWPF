0e6eae3466d1b6fc258fccf3fc3164bca5474e9b
Hai Doan Duc <beu2901@gmail.com>
2025-07-24 12:38:46 +0700

    link stock management window w other windows

--- FarmProductsWPF/StockManagementWindow.xaml.cs
+++ FarmProductsWPF/StockManagementWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FarmProductsWPF_BOs;
+using FarmProductsWPF_Repositories.Implements;
+using FarmProductsWPF_Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +22,42 @@ namespace FarmProductsWPF
     /// </summary>
     public partial class StockManagementWindow : Window
     {
-        public StockManagementWindow()
+        private Account _user;
+        private readonly IStockRepo _stockRepo;
+
+        public Account CurrentUser
+        {
+            get { return _user; }
+            private set { _user = value; }
+        }
+
+        public StockManagementWindow(Account account)
         {
             InitializeComponent();
+            _user = account;
+            this.DataContext = this;
+            _stockRepo = new StockRepo();
+        }
+
+        private void btnViewStockWindow_Click(object sender, RoutedEventArgs e)
+        {
+            FarmProductManagementWindow viewStockWindow = new FarmProductManagementWindow(_user);
+            viewStockWindow.Show();
+            this.Close();
+        }
+
+        private void btnCreateOrderWindow_Click(object sender, RoutedEventArgs e)
+        {
+            CreateOrderWindow createOrderWindow = new CreateOrderWindow(_user);
+            createOrderWindow.Show();
+            this.Close();
+        }
+
+        private void btnOrderHistoryWindow_Click(object sender, RoutedEventArgs e)
+        {
+            OrderHistoryWindow orderHistoryWindow = new OrderHistoryWindow(_user);
+            orderHistoryWindow.Show();
+            this.Close();
         }
     }
 }
