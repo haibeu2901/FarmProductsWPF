947d672fd2ac688a3a19948c741906f71d20f80c
Hai Doan Duc <beu2901@gmail.com>
2025-07-24 15:43:47 +0700

    Category CRUD

---
+++ FarmProductsWPF_Repositories/Implements/CategoryRepo.cs
@@ -0,0 +1,44 @@
+﻿using FarmProductsWPF_BOs;
+using FarmProductsWPF_DAOs;
+using FarmProductsWPF_Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmProductsWPF_Repositories.Implements
+{
+    public class CategoryRepo : ICategoryRepo
+    {
+        public ProductCategory AddCategory(ProductCategory category)
+        {
+            return CategoryDAO.Instance.AddCategory(category);
+        }
+
+        public bool DeleteCategory(int categoryId)
+        {
+            return CategoryDAO.Instance.DeleteCategory(categoryId);
+        }
+
+        public List<ProductCategory> GetAllCategories()
+        {
+            return CategoryDAO.Instance.GetAllCategories();
+        }
+
+        public ProductCategory? GetCategoryById(int categoryId)
+        {
+            return CategoryDAO.Instance.GetCategoryById(categoryId);
+        }
+
+        public List<ProductCategory> SearchCategory(string searchText)
+        {
+            return CategoryDAO.Instance.SearchCategory(searchText);
+        }
+
+        public ProductCategory UpdateCategory(ProductCategory category)
+        {
+            return CategoryDAO.Instance.UpdateCategory(category);
+        }
+    }
+}
---
+++ FarmProductsWPF_Repositories/Interfaces/ICategoryRepo.cs
@@ -0,0 +1,19 @@
+﻿using FarmProductsWPF_BOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmProductsWPF_Repositories.Interfaces
+{
+    public interface ICategoryRepo
+    {
+        bool DeleteCategory(int categoryId);
+        ProductCategory UpdateCategory(ProductCategory category);
+        ProductCategory AddCategory(ProductCategory category);
+        ProductCategory? GetCategoryById(int categoryId);
+        List<ProductCategory> SearchCategory(string searchText);
+        List<ProductCategory> GetAllCategories();
+    }
+}
