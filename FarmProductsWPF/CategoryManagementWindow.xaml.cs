30a1173019e6ab9caa5bc9d51331b8bc85af7d46
Hai Doan Duc <beu2901@gmail.com>
2025-07-24 15:34:00 +0700

    init and link Category management window w others

---
+++ FarmProductsWPF/CategoryManagementWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using FarmProductsWPF_BOs;
+using FarmProductsWPF_Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FarmProductsWPF
+{
+    /// <summary>
+    /// Interaction logic for CategoryManagementWindow.xaml
+    /// </summary>
+    public partial class CategoryManagementWindow : Window
+    {
+        private Account _user;
+
+        public Account CurrentUser
+        {
+            get { return _user; }
+            private set { _user = value; }
+        }
+
+        public CategoryManagementWindow(Account account)
+        {
+            InitializeComponent();
+            _user = account;
+            this.DataContext = this;
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow login = new LoginWindow();
+            this.Close();
+            login.Show();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnViewStockWindow_Click(object sender, RoutedEventArgs e)
+        {
+            FarmProductManagementWindow farmProductManagementWindow = new FarmProductManagementWindow(_user);
+            farmProductManagementWindow.Show();
+            this.Close();
+        }
+
+        private void btnCreateOrderWindow_Click(object sender, RoutedEventArgs e)
+        {
+            CreateOrderWindow createOrderWindow = new CreateOrderWindow(_user);
+            createOrderWindow.Show();
+            this.Close();
+        }
+
+        private void btnOrderHistoryWindow_Click(object sender, RoutedEventArgs e)
+        {
+            OrderHistoryWindow orderHistoryWindow = new OrderHistoryWindow(_user);
+            orderHistoryWindow.Show();
+            this.Close();
+        }
+
+        private void btnStockManagementWindow_Click(object sender, RoutedEventArgs e)
+        {
+            StockManagementWindow stockManagementWindow = new StockManagementWindow(_user);
+            stockManagementWindow.Show();
+            this.Close();
+        }
+    }
+}
